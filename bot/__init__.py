95b350b2734238fbc0baed524c2c215b86601dd4
irfanalibot0 <104135320+irfanalibot0@users.noreply.github.com>
2022-06-09 18:34:21 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,3 +1,8 @@
+import os
+from collections import defaultdict
+import logging
+from logging.handlers import RotatingFileHandler
+import time
 from logging import getLogger, FileHandler, StreamHandler, INFO, basicConfig, error as log_error, info as log_info, warning as log_warning
 from socket import setdefaulttimeout
 from faulthandler import enable as faulthandler_enable
@@ -26,6 +31,40 @@ botStartTime = time()
 basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     handlers=[FileHandler('log.txt'), StreamHandler()],
                     level=INFO)
+
+"""Some Constants"""
+UPLOAD_AS_DOC = {}  # Maintain each user ul_type
+UPLOAD_TO_DRIVE = {}  # Maintain each user drive_choice
+
+FINISHED_PROGRESS_STR = os.environ.get("FINISHED_PROGRESS_STR", "█")
+UN_FINISHED_PROGRESS_STR = os.environ.get("UN_FINISHED_PROGRESS_STR", "░")
+EDIT_SLEEP_TIME_OUT = 10
+gDict = defaultdict(lambda: [])
+queueDB = {}
+formatDB = {}
+replyDB = {}
+
+
+w = open('Merge-Bot.txt','w')
+w.truncate(0)
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(levelname)s - %(message)s [%(filename)s:%(lineno)d]",
+    datefmt="%d-%b-%y %H:%M:%S",
+    handlers=[
+        RotatingFileHandler("Merge-Bot.txt", maxBytes=50000000, backupCount=10),
+        logging.StreamHandler(),
+    ],
+)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+logging.getLogger("urllib3").setLevel(logging.WARNING)
+logging.getLogger("PIL").setLevel(logging.WARNING)
+
+LOGGER = logging.getLogger(__name__)
+BROADCAST_MSG = '''
+**Total: {}
+Done: {}**
+'''
 
 LOGGER = getLogger(__name__)
 
